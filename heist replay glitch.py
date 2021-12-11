50ae4406a79245faf2c2d0abda7528c89cb2633d
Blur <dariusalexa12@gmail.com>
2021-12-11 17:47:29 +0200

    Updated Heist Replay Glitch for easier netwrok interface name modification

--- heist replay glitch.py
+++ heist replay glitch.py
@@ -7,15 +7,20 @@ import keyboard
 
 toggle = False
 
+with open("settings.cfg", 'r') as f:
+    settings = f.readlines()
+
+nsetting = [setting.strip().split(' ') for setting in settings]
+
 print("Heist Replay Glitch | PRESS N")
 
 while True:
     if keyboard.is_pressed("n"):
         if toggle == False:
             print('N presed. Stopping connection')
-            os.system("netsh interface set interface Ethernet disabled") # Change "Ethernet" for your network interface's name
+            os.system(f"netsh interface set interface {nsetting[0][0]} disabled")
             toggle = True
         else:
             print('Restoring connection')
-            os.system("netsh interface set interface Ethernet enabled") # Change "Ethernet" for your network interface's name
+            os.system(f"netsh interface set interface {nsetting[0][0]} enabled")
             toggle = False
